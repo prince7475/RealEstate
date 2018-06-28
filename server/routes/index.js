15c4e05991d21627d82fc8c67cf6545e7f60a615
prince7475 <prince.donkor74@gmail.com>
2018-06-28 08:04:51 -0400

    created data

---
+++ server/routes/index.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const Hotel = require("../models").Hotel;
+const Restaurant = require("../models").Restaurant;
+const Activity = require("../models").Activity;
+
+// GET /api/
+router.get("/", (req, res, next) => {
+  Promise.all([
+    Hotel.findAll({ include: [{ all: true }] }),
+    Restaurant.findAll({ include: [{ all: true }] }),
+    Activity.findAll({ include: [{ all: true }] })
+  ])
+    .then(([hotels, restaurants, activities]) => {
+      res.json({
+        hotels,
+        restaurants,
+        activities
+      });
+    })
+    .catch(next);
+});
+
+module.exports = router;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,9 @@
 module.exports = {
-  entry: ['babel-polyfill', './client/index.js'],
+  entry: './client/index.js', // the starting point for our program
   output: {
-    path: __dirname,
-    filename: './public/bundle.js'
+    path: __dirname + '/public', // the absolute path for the directory where we want the output to be placed
+    filename: 'bundle.js' // the name of the file that will contain our output - we could name this whatever we want, but bundle.js is typical
   },
-  context: __dirname,
-  devtool: 'source-maps',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+  devtool: "source-map",
 }
+
